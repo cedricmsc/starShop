57a8dc657405a9e7b31ec8585bf037d0f7f1911e
cedricmsc <kouetosine@yahoo.fr>
2024-07-14 02:04:50 +0200

    adding last changes about cs-fixus-shim

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,14 +1,15 @@
 <?php
+
 namespace App\Controller;
 
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MainController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-      return new response('<strong>Starshop</strong>: your monopy-busting option for Starship parts!');
+        return new Response('<strong>Starshop</strong>: your monopy-busting option for Starship parts!');
     }
 }
