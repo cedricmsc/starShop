b9a12fda268f91f23b074c3a82d772226038fccb
cedricmsc <kouetosine@yahoo.fr>
2024-07-14 03:11:50 +0200

    twig - filter and other

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,12 +4,24 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopy-busting option for Starship parts!');
+        $numberOfStarship = 442;
+        $myShip = [
+            'name' => 'Uss LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Eric Mabiala',
+            'status' => 'Under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'starship' => $numberOfStarship,
+            'myShip' => $myShip,
+        ]);
     }
 }
